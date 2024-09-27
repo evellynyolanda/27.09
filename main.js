4a49217d842fb76fcc5587767c9d607785ecff2e
evellynyolanda <140424998+evellynyolanda@users.noreply.github.com>
2024-09-27 08:18:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+numeroSenha.textContent = 5;
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+botoes[0].onclick = diminuiTamanho;
+
+console.log(botoes);
